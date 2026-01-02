95bc227ea8825966adfaf177af2505dd0db71a21
Ankit Sharma <ankitsharma2615@gmail.com>
2026-01-02 18:35:07 +0530

    fix: cache for Initial deployment

--- app/(shared-layout)/blog/page.tsx
+++ app/(shared-layout)/blog/page.tsx
@@ -1,15 +1,16 @@
 import Link from "next/link";
 import Image from "next/image";
 import { Metadata } from "next";
-import { Suspense } from "react";
-import { cacheTag } from "next/cache";
+// import { Suspense } from "react";
+// import { cacheLife, cacheTag } from "next/cache";
 
 import { fetchQuery } from "convex/nextjs";
 import { api } from "@/convex/_generated/api";
 
-import { Skeleton } from "@/components/ui/skeleton";
+// import { Skeleton } from "@/components/ui/skeleton";
 import { buttonVariants } from "@/components/ui/button";
 import { Card, CardContent, CardFooter } from "@/components/ui/card";
+import { connection } from "next/server";
 
 export const metadata: Metadata = {
   title: "Blog | Nextjs 16 Tutorial",
@@ -37,8 +38,10 @@ export default function BlogPage() {
 }
 
 async function LoadBlogList() {
-  "use cache";
-  cacheTag("blog");
+  // "use cache";
+  // cacheLife("hours");
+  // cacheTag("blog");
+  await connection();
   const data = await fetchQuery(api.posts.getPosts);
 
   return (
@@ -80,19 +83,19 @@ async function LoadBlogList() {
   );
 }
 
-function SkeletonLoadingUI() {
-  return (
-    <div className="grid gap-6 md:grid-cols-3 lg:grid-cols-3">
-      {[...Array(6)].map((_, i) => (
-        <div className="flex flex-col space-y-3" key={i}>
-          <Skeleton className="h-48 w-full rounded-xl" />
-          <div className="space-y-2 flex flex-col">
-            <Skeleton className="h-4 w-3/4" />
-            <Skeleton className="h-4 w-full" />
-            <Skeleton className="h-6 w-2/3" />
-          </div>
-        </div>
-      ))}
-    </div>
-  );
-}
+// function SkeletonLoadingUI() {
+//   return (
+//     <div className="grid gap-6 md:grid-cols-3 lg:grid-cols-3">
+//       {[...Array(6)].map((_, i) => (
+//         <div className="flex flex-col space-y-3" key={i}>
+//           <Skeleton className="h-48 w-full rounded-xl" />
+//           <div className="space-y-2 flex flex-col">
+//             <Skeleton className="h-4 w-3/4" />
+//             <Skeleton className="h-4 w-full" />
+//             <Skeleton className="h-6 w-2/3" />
+//           </div>
+//         </div>
+//       ))}
+//     </div>
+//   );
+// }
